307212825efd825c8814642353a419f3c2e4aa42
SurajNair77 <131618542+SurajNair77@users.noreply.github.com>
2026-04-06 15:44:56 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@ const isNeon = process.env.DB_HOST?.includes('neon.tech')
 
 const db = new Pool({
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
